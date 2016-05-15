2609a2bbaaae7d0a6467bb8073e54b626cfd2fe1
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2016-05-15 14:11:51 +0200

    Added settings files in project directory. Set Copy-To-Output to Preserve Newst on these files.

---
+++ Cornelius/Beállítások/csoportositas-informatikus-bsc.gd
@@ -0,0 +1,254 @@
+﻿CSOPORTOSÍTÁS {
+	CSOPORT Kötelező {
+		BMEGT20A001, /* Menedzsment és vállalkozásgazdaságtan */
+		BMEGT301920, /* Közgazdaságtan I. */
+		BMEGT301921, /* Közgazdaságtan II. */
+		BMEGT30A001, /* Mikro- és makroökonómia */
+		BMEGT55A001, /* Üzleti jog */
+		BMETE111211, /* Fizika C1i */
+		BMETE111212, /* Fizika C2i */
+		BMETE111820, /* Fizika C1 */
+		BMETE111821, /* Fizika C2 */
+		BMETE11AX03, /* Fizika 1i */
+		BMETE11AX04, /* Fizika 2i */
+		BMETE901913, /* Matematika B1 */
+		BMETE901918, /* Matematika B2 */
+		BMETE90AX04, /* Analízis 1 informatikusoknak */
+		BMETE90AX05, /* Analízis 2 informatikusoknak */
+		BMETE90MX42, /* Felsőbb matematika informatikusoknak C */
+		BMETE911708, /* Matematikai logika */
+		BMETE921029, /* Analízis szigorlat informatikusoknak */
+		BMETE921244, /* Analízis I. */
+		BMETE921567, /* Analízis 2 informatikusoknak */
+		BMEVIAUA218, /* Szoftvertechnikák */
+		BMEVIAUA309, /* Szabályozástechnika */
+		BMEVIEE1226, /* A programozás alapjai I. */
+		BMEVIEE1227, /* A programozás alapjai II. */
+		BMEVIEE1233, /* Számítógép-laboratórium I. */
+		BMEVIEE1234, /* Számítógép-laboratórium II. */
+		BMEVIEE1237, /* Számítógép-laboratórium II. */
+		BMEVIEE1238, /* Számítógépek programozása laboratórium l. */
+		BMEVIEE1239, /* A programozás alapjai I. */
+		BMEVIEE1240, /* Programozás alapjai II. */
+		BMEVIEE2219, /* Elektronika */
+		BMEVIEE2240, /* Elektronik */
+		BMEVIEEA100, /* A programozás alapjai I. */
+		BMEVIEEA101, /* Szoftver Labor I. */
+		BMEVIEEA112, /* A programozás alapjai II. */
+		BMEVIEEA113, /* Szoftver Labor II */
+		BMEVIEEA307, /* Elektronika */
+		BMEVIEV2217, /* Jelek és rendszerek */
+		BMEVIEV2239, /* Jelek és rendszerek (Signale und Systeme) */
+		BMEVIFO1503, /* Digitaltechnik */
+		BMEVIFO2218, /* Deklaratív programozás */
+		BMEVIFO2228, /* A programozás technológiája */
+		BMEVIFO2236, /* Számítógép-laboratórium IV. */
+		BMEVIFO2237, /* Számítógép laboratórium III. */
+		BMEVIFO2238, /* Digitaltechnik */
+		BMEVIFO2511, /* Digitaltechnik szigorlat */
+		BMEVIFO3222, /* Számítógép-hálózatok */
+		BMEVIHI2221, /* Számítógép-architektúrák */
+		BMEVIHI3210, /* Kódelmélet */
+		BMEVIHI4363, /* Adatbiztonság */
+		BMEVIHIA209, /* Kódolástechnika */
+		BMEVIHIA210, /* Számítógép-architektúrák */
+		BMEVIHIA215, /* Számítógép-hálózatok */
+		BMEVIHIM102, /* Adatbiztonság */
+		BMEVIHVA214, /* Jelek és rendszerek */
+		BMEVIIIA108, /* Digitális technika II. */
+		BMEVIIIA114, /* A programozás alapjai 2 */
+		BMEVIIIA115, /* Szoftver laboratórium 2 */
+		BMEVIIIA212, /* Szoftver laboratórium 3 */
+		BMEVIIIA217, /* Szoftvertechnológia */
+		BMEVIIIA220, /* Szoftver laboratórium 4 */
+		BMEVIIIA316, /* Számítógépes grafika és képfeldolgozás */
+		BMEVIMA1235, /* Bevezetés a Számításelméletbe I. */
+		BMEVIMA1236, /* Bevezetés a számításelméletbe II. */
+		BMEVIMA2203, /* Valószínűségszámítás */
+		BMEVIMA2204, /* Tömegkiszolgálás */
+		BMEVIMA2207, /* Algoritmusok elmélete */
+		BMEVIMA2208, /* Formális nyelvek */
+		BMEVIMA2512, /* Wahrscheinlichkeitsrechnung */
+		BMEVIMA2602, /* Formale Sprachen */
+		BMEVIMA2603, /* Einführung in die theoretische Information II. */
+		BMEVIMA2605, /* Massenbedienung */
+		BMEVIMA2607, /* Theorie der Algorithmen */
+		BMEVIMA3209, /* Információelmélet */
+		BMEVIMA3232, /* Adatbázisok */
+		BMEVIMA3239, /* Számításelmélet szigorlat */
+		BMEVIMH1506, /* Programmplanung I. */
+		BMEVIMH1507, /* Programmplanung II. */
+		BMEVIMIA102, /* Digitális technika I. */
+		BMEVIMIA111, /* Digitális technika II. */
+		BMEVIMIA211, /* Mérés laboratórium 1. */
+		BMEVIMIA216, /* Mérés laboratórium 2. */
+		BMEVIMIA219, /* Operációs rendszerek */
+		BMEVIMIA312, /* Mérés laboratórium 3. */
+		BMEVIMIA313, /* Mesterséges intelligencia */
+		BMEVIMIA315, /* Mérés laboratórium 4 */
+		BMEVIMIA401, /* Rendszermodellezés */
+		BMEVIMM1219, /* Digitális technika I. */
+		BMEVIMM1220, /* Digitális technika II. */
+		BMEVIMM2212, /* Digitális rendszerek szigorlat */
+		BMEVIMM2224, /* Mérés laboratórium 1. */
+		BMEVIMM2508, /* Informatik IC1 */
+		BMEVIMM3231, /* Operációs rendszerek */
+		BMEVIMA1602, /* Einführung in die theoretische Informatik I. */
+		BMEVISZA103, /* Bevezetés a számításelméletbe 1. */
+		BMEVISZA110, /* Bevezetés a számításelméletbe 2. */
+		BMEVISZA208, /* Valószínűségszámítás */
+		BMEVISZA213, /* Algoritmuselmélet */
+		BMEVISZA402, /* Deklaratív programozás */
+		BMEVISZM101, /* Információelmélet */
+		BMEVISZM104, /* Nyelvek és automaták */
+		BMEVISZM106, /* Tömegkiszolgálás */
+		BMEVITMA308, /* Szoftver laboratórium 5 */
+		BMEVITMA310, /* Távközlő hálózatok és szolgáltatások */
+		BMEVITMA311, /* Adatbázisok */
+		BMEVITMA314, /* Információs rendszerek üzemeltetése */
+		BMEVITMA400, /* Beszéd-információs rendszerek */
+
+		BMETE90AX21, /* Analízis 1 informatikusoknak */
+		BMETE90AX22, /* Analízis 2 informatikusoknak */
+		BMEVISZAB00, /* Valószínűségszámítás */
+		BMEVISZAA00, /* Bevezetés a számításelméletbe 1 */
+		BMEVISZAA01, /* Bevezetés a számításelméletbe 2 */
+		BMEVIHIAB00, /* Kódolástechnika */
+		BMEVISZAB01, /* Algoritmuselmélet */
+		BMETE11AX23, /* Fizika 1i */
+		BMETE11AX24, /* Fizika 2i */
+		BMEGT30A001, /* Mikro- és makroökonomia */
+		BMEGT20A001, /* Menedzsment és vállalkozásgazdaságtan */
+		BMEGT55A001, /* Üzleti jog */
+		BMEVIHVAB00, /* Rendszerelmélet  */
+		BMEVIHIAA00, /* Számítógép-architektúrák */
+		BMEVIHIAB01, /* Kommunikációs hálózatok 1 */
+		BMEVITMAB01, /* Kommunikációs hálózatok 2 */
+		BMEVIMIAB00, /* Operációs rendszerek */
+		BMEVIEEAA00, /* A programozás alapjai 1 */
+		BMEVIIIAA00, /* A programozás alapjai 2 */
+		BMEVIIIAB00, /* A programozás alapjai 3 */
+		BMEVITMAB00, /* Adatbázisok */
+		BMEVITMAB02, /* Adatbázisok laboratórium  */
+		BMEVIIIAB01, /* Szoftvertechnológia */
+		BMEVIAUAB00, /* Szoftvertechnikák */
+		BMEVIIIAB02, /* Szoftver projekt laboratórium */
+		BMEVIIIAB03, /* Számítógépes grafika */
+		BMEVIMIAC00, /* Mesterséges intelligencia */
+		BMEVITMAC02, /* Információs rendszerek üzemeltetése */
+
+		
+		/* Saját kiegészítések */
+		BMEVIFO4351, /* Számítógépes grafika és képfeldolgozás */
+		BMEVIAU3215, /* Szabályozástechnika */
+		BMEVIMM3226, /* Mérés laboratórium II. */
+		BMEVIMIA405, /* Rendszermodellezés */
+		
+		/* Bevezető tárgyak */
+		BMETE11AX12, /* Bevezető fizika */
+		BMETE90AX40  /* Bevezető matematika */
+	},
+	TÚLCSORDULÓ CSOPORT KötelezőenVálasztható {
+		/* 2007/2008/1 félévig számító kötelezően választható tárgyak */
+		BMEGT20A002 (       - 2007/1), /* Marketing */
+		BMEGT20A017 (       - 2007/1), /* Innovációmenedzsment */
+		BMEGT33A002 (       - 2007/1), /* Marketing */
+		BMEGT35A001 (       - 2007/1), /* Pénzügyek */
+		BMEGT35A002 (       - 2007/1), /* Számvitel */
+		BMEGT35A003 (       - 2007/1), /* Gazdaságpolitika */
+		BMEGT41A001 (       - 2007/1), /* Filozófia */
+		BMEGT41A002 (       - 2007/1), /* Kutatásmódszertan */
+		BMEGT42A001 (       - 2007/1), /* Környezetgazdaságtan */
+		BMEGT42A002 (       - 2007/1), /* Regionális gazdaságtan */
+		BMEGT43A001 (       - 2007/1), /* Kommunikáció */
+		BMEGT43A002 (       - 2007/1), /* Szociológia */
+		BMEGT43A022 (       - 2007/1), /* Művészettörténet II. */
+		BMEGT44A001 (       - 2007/1), /* Innovációmenedzsment */
+		BMEGT46A001 (       - 2007/1), /* Információmenedzsment */
+		BMEGT47A001 (       - 2007/1), /* Látás, nyelv, emlékezet */
+		BMEGT51A001 (       - 2007/1), /* Digitális Pedagógia */
+		BMEGT52A001 (       - 2007/1), /* Ergonómia */
+		BMEGT52A002 (       - 2007/1), /* Pszichológia */
+		BMEVITMAK47 (       - 2007/1), /* Mérnöki menedzsment módszerek */
+		
+		/* 2007/2008/1 félévig számító kredites nyelvi tárgyak */
+		BMEGT61A051 (       - 2007/1), /* Műszaki nyelv - német */
+		BMEGT61A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - német */
+		BMEGT61A081 (       - 2007/1), /* Menedzserkommunikáció - német */
+		BMEGT61A091 (       - 2007/1), /* Kultúraközi kommunikáció - német */
+		BMEGT62AF51 (       - 2007/1), /* Műszaki nyelv - francia */
+		BMEGT62AF61 (       - 2007/1), /* Kommunikációs készségfejlesztés - francia */
+		BMEGT62AF81 (       - 2007/1), /* Menedzserkommunikáció – francia */
+		BMEGT62AF91 (       - 2007/1), /* Kultúraközi kommunikáció - francia */
+		BMEGT62AO51 (       - 2007/1), /* Műszaki nyelv - olasz */
+		BMEGT62AO61 (       - 2007/1), /* Kommunikációs készségfejlesztés - olasz */
+		BMEGT62AO81 (       - 2007/1), /* Menedzserkommunikáció - olasz */
+		BMEGT62AO91 (       - 2007/1), /* Kultúraközi kommunikáció - olasz */
+		BMEGT62AS61 (       - 2007/1), /* Kommunikációs készségfejlesztés - spanyol */
+		BMEGT62AS81 (       - 2007/1), /* Menedzserkommunikáció - spanyol */
+		BMEGT62AS91 (       - 2007/1), /* Kultúraközi kommunikáció - spanyol */
+		BMEGT63A051 (       - 2007/1), /* Műszaki nyelv - angol */
+		BMEGT63A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - angol */
+		BMEGT63A081 (       - 2007/1), /* Kultúraközi kommunikáció - angol */
+		BMEGT63A091 (       - 2007/1), /* Kultúraközi kommunikáció - angol */
+		BMEGT65A051 (       - 2007/1), /* Műszaki nyelv - magyar */
+		BMEGT65A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - magyar */
+		BMEGT65A091 (       - 2007/1), /* Kultúraközi kommunikáció - magyar */
+		
+		/* 2007/2008/2 - 2010/2011/2 között érvényes tárgyak*/
+		BMEGT35A001 (2007/2 - 2010/2), /* Pénzügyek */
+		BMEGT47A001 (2007/2 - 2010/2), /* Látás, nyelv, emlékezet */
+		BMEGT35A003 (2007/2 - 2010/2), /* Gazdaságpolitika */
+		BMEGT42A001 (2007/2 - 2010/2), /* Környezetgazdaságtan */
+		BMEGT41A002 (2007/2 - 2010/2), /* Kutatásmódszertan */
+		BMEGT35A002 (2007/2 - 2010/2), /* Számvitel */
+		BMEVITMAK47 (2007/2 - 2010/2), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2007/2 - 2010/2), /* Marketing */
+		BMEGT52A002 (2007/2 - 2010/2), /* Pszichológia */
+		BMEGT52A001 (2007/2 - 2010/2), /* Ergonómia */
+		BMEGT43A002 (2007/2 - 2010/2), /* Szociológia */
+		BMEVIETAK49 (2007/2 - 2010/2), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2007/2 - 2010/2), /* Mérnöki problémamegoldás */
+		BMEGT51A001 (2007/2 - 2010/2), /* Digitális Pedagógia */
+		
+		/* 2011/2012/1 félévben érvényes tárgyak */
+		BMEGT35A001 (2011/1 - 2011/1), /* Pénzügyek */
+		BMEGT20V100 (2011/1 - 2011/1), /* Innovatív vállalkozások indítása és működtetése */
+		BMEGT35A003 (2011/1 - 2011/1), /* Gazdaságpolitika */
+		BMEGT42A001 (2011/1 - 2011/1), /* Környezetgazdaságtan */
+		BMEGT52A013	(2011/1 - 2011/1), /* Szociális készségfejlesztés */
+		BMEGT35A002 (2011/1 - 2011/1), /* Számvitel */
+		BMEVITMAK47 (2011/1 - 2011/1), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2011/1 - 2011/1), /* Marketing */
+		BMEGT52A002 (2011/1 - 2011/1), /* Pszichológia */
+		BMEGT52A001 (2011/1 - 2011/1), /* Ergonómia */
+		BMEGT43A002 (2011/1 - 2011/1), /* Szociológia */
+		BMEVIETAK49 (2011/1 - 2011/1), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2011/1 - 2011/1), /* Mérnöki problémamegoldás */
+		
+		/* 2011/2012/2 félévtől érvényes tárgyak */
+		BMEGT35A001 (2011/2 -       ), /* Pénzügyek */
+		BMEGT20V100 (2011/2 -       ), /* Innovatív vállalkozások indítása és működtetése */
+		BMEGT35A003 (2011/2 -       ), /* Gazdaságpolitika */
+		BMEGT42A001 (2011/2 -       ), /* Környezetgazdaságtan */
+		BMEGT52A013	(2011/2 -       ), /* Szociális készségfejlesztés */
+		BMEGT35A002 (2011/2 -       ), /* Számvitel */
+		BMEVITMAK47 (2011/2 -       ), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2011/2 -       ), /* Marketing */
+		BMEGT52A002 (2011/2 -       ), /* Pszichológia */
+		BMEGT52A001 (2011/2 -       ), /* Ergonómia */
+		BMEGT43A002 (2011/2 -       ), /* Szociológia */
+		BMEVIETAK49 (2011/2 -       ), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2011/2 -       ), /* Mérnöki problémamegoldás */
+		BMEVITMAK48 (2011/2 -       ), /* Érzelmek logikája */
+		BMEVITMAK49 (2011/2 -       ), /* Digitális életmód */
+		
+		/* Dénes féle kiegészítések, amik nincsenek fenn a kari honlapon */
+		BMEGT419021 (       - 2004/1), /* Érveléstechnika - logika */
+		BMEGT419733 (       - 2004/1), /* Filozófia */
+		BMEGT431427 (       - 2007/1), /* Művészettörténet II. (Impresszionizmustól a napjainkig) */
+		BMEGT628F91 (       - 2007/1), /* Francia nyelvi és civilizációs háttérismeretek - Filiere I. */
+		BMETE47A001 (       - 2010/2)  /* Látás, nyelv, emlékezet */
+	},
+	ALAPÉRTELMEZETT CSOPORT SzabadonVálasztható
+};
---
+++ Cornelius/Beállítások/csoportositas-villamosmernok-bsc.gd
@@ -0,0 +1,236 @@
+﻿CSOPORTOSÍTÁS {
+	CSOPORT Kötelező {
+		BMEGEMT1107, /* Werkstoffkunde für Elektroingenieure */
+		BMEGEMT1532, /* Anyagtudomány */
+		BMEGEMTAV01, /* Anyagtudomány */
+		BMEGT20A001, /* Menedzsment és vállalkozásgazdaságtan */
+		BMEGT301920, /* Közgazdaságtan I. */
+		BMEGT301921, /* Közgazdaságtan II. */
+		BMEGT30A001, /* Mikro- és makroökonómia */
+		BMEGT55A001, /* Üzleti jog */
+		BMETE111820, /* Fizika C1 */
+		BMETE111821, /* Fizika C2 */
+		BMETE111822, /* Fizika C3 */
+		BMETE11AX01, /* Fizika 1 */
+		BMETE11AX02, /* Fizika 2 */
+		BMETE11AX02, /* Fizika 2 */
+		BMETE11MX01, /* Fizika 3 */
+		BMETE901913, /* Matematika B1 */
+		BMETE901918, /* Matematika B2 */
+		BMETE901919, /* Matematika B3 */
+		BMETE902923, /* Matematika B4 */
+		BMETE90AX00, /* Matematika A1a - Analízis */
+		BMETE90AX02, /* Matematika A2a - Vektorfüggvények */
+		BMETE90AX08, /* Matematika A4 - Valószínűségszámítás */
+		BMETE90AX09, /* Matematika A3 villamosmérnököknek */
+		BMETE921568, /* Matematika szigorlat villamosmérnököknek */
+		BMETE922246, /* Matematika B3* */
+		BMEVIAU2023, /* Informatika I. */
+		BMEVIAU2024, /* Informatika II. */
+		BMEVIAU3028, /* Szabályozástechnika */
+		BMEVIAUA116, /* A programozás alapjai 2 */
+		BMEVIAUA203, /* Informatika 2. */
+		BMEVIAUA300, /* Elektronika 2 */
+		BMEVIEE2019, /* Elektronika I. */
+		BMEVIEE2241, /* Elektronik I. */
+		BMEVIEEA306, /* Mikroelektronika */
+		BMEVIET1015, /* Programtervezés I. */
+		BMEVIET1017, /* Programtervezés II. */
+		BMEVIET3029, /* Elektronikai technológia */
+		BMEVIETA302, /* Elektronikai technológia */
+		BMEVIEV1014, /* Hálózatok és rendszerek 1. (Networks and Systems 1.) */
+		BMEVIEV1019, /* Hálózatok és rendszerek 1. (Networks and Systems 1.) */
+		BMEVIEV1505, /* Hálózatok és rendszerek I. (Netzwerke, Signale und Systeme I.) */
+		BMEVIEV2007, /* Hálózatok és rendszerek szigorlat */
+		BMEVIEV2015, /* Hálózatok és rendszerek 2. (Networks and Systems 2.) */
+		BMEVIEV2018, /* Elektromágneses terek */
+		BMEVIEV2021, /* Hálózatok és rendszerek II. */
+		BMEVIEV2501, /* Hálózatok és rendszerek II. (Netzwerke, Signale und Systeme II.) */
+		BMEVIEV2513, /* Netzwerke und Systeme szigorlat */
+		BMEVIEV2551, /* Elektromagnetische Feldtheorie */
+		BMEVIFO1013, /* Digitális technika */
+		BMEVIFO1016, /* Digitális technika */
+		BMEVIFO1020, /* Digitális technika II. (Digital Design) */
+		BMEVIFO1503, /* Digitaltechnik */
+		BMEVIFO1504, /* Digitaltechnik */
+		BMEVIFO2006, /* Digitális technika szigorlat */
+		BMEVIFO2023, /* Informatika I. */
+		BMEVIFO2511, /* Digitaltechnik szigorlat */
+		BMEVIFO2553, /* Messtechnik */
+		BMEVIHIA106, /* A programozás alapjai I. */
+		BMEVIHIA205, /* Elektronika 1 */
+		BMEVIHVA109, /* Jelek és rendszerek 1. */
+		BMEVIHVA200, /* Jelek és rendszerek 2. */
+		BMEVIHVA204, /* Elektromágneses terek */
+		BMEVIIIA104, /* Digitális technika I. */
+		BMEVIIIA108, /* Digitális technika II. */
+		BMEVIIIA202, /* Informatika 1 */
+		BMEVIIIA303, /* Szabályozástechnika */
+		BMEVIMA2025, /* A számítástudomány alapjai */
+		BMEVIMA2026, /* A számítástudomány alapjai */
+		BMEVIMA2512, /* Wahrscheinlichkeitsrechnung */
+		BMEVIMA2555, /* Grundlagen der theoretischen Informatik */
+		BMEVIMH1506, /* Programmplanung I. */
+		BMEVIMH1507, /* Programmplanung II. */
+		BMEVIMIA206, /* Méréstechnika */
+		BMEVIMIA304, /* Laboratórium 1. */
+		BMEVIMIA305, /* Laboratórium 2. */
+		BMEVIMM2022, /* Méréstechnika */
+		BMEVIMM2508, /* Informatik IC1 */
+		BMEVIMM2554, /* Informatik IC2 */
+		BMEVISZA105, /* A számítástudomány alapjai */
+		BMEVITMA301, /* Infokommunikáció */
+		BMEVITT2554, /* Informatik II. */
+		BMEVITT3026, /* Híradástechnika */
+		BMEVIVE2020, /* Elektrotechnika I. */
+		BMEVIVE2027, /* Elektrotechnika II. */
+		BMEVIVEA201, /* Elektrotechnika */
+		BMEVIVEA207, /* Villamos energetika */
+		BMEVIVM3025, /* Villamos energetika */
+
+		TE90AX00 , /* Matematika A1 */
+		TE90AX26, /* Matematika A2 */
+		TE90AX09, /* Matematika A3 */
+		TE90AX51, /* Matematika A4 */
+		TE11AX21, /* Fizika 1 */
+		TE11AX22, /* Fizika 2 */
+		VISZAA02, /* A számítástudomány alapjai */
+		VIIIAB04, /* Informatika 1 */
+		VIAUAB01, /* Informatika 2 */
+		GT30A001, /* Mikro- és makroökonomia */
+		GT20A001, /* Menedzsment és vállalkozásgazdaságtan */
+		GT55A001, /* Üzleti jog */
+		VIHIAA01, /* A programozás alapjai 1 */
+		VIAUAA00, /* A programozás alapjai 2 */
+		VIIIAA01, /* Digitális technika 1 */
+		VIIIAA02, /* Digitális technika 2 */
+		VIHVAA00, /* Jelek és rendszerek 1 */
+		VIHVAB01, /* Jelek és rendszerek 2 */
+		VIVEAB00, /* Elektrotechnika */
+		VIHVAC03, /* Elektromágneses terek alapjai */
+		VIHIAB02, /* Elektronika 1 */
+		VIAUAC05, /* Elektronika 2 */
+		VIMIAB01, /* Méréstechnika */
+		VIMIAC05, /* Laboratórium 1 */
+		VIMIAC07, /* Laboratórium 2 */
+		VIIIAB05, /* Szabályozástechnika */
+		VITMAB03, /* Infokommunikáció */
+		VIEEAB00, /* Mikroelektronika */
+		VIVEAB01, /* Villamos energetika */
+
+		
+		/* Saját kiegészítések */
+		BMEVIHI3020, /* Elektronika II. */
+		BMEVIHIA107, /* A programozás alapjai 2. */
+		BMEVIIIA105, /* Digitális technika 1 */
+		BMEVIIIA106, /* Digitális technika 2 */
+		BMEVIHVA201, /* Elektromágneses terek alapjai */
+		
+		/* Bevezető tárgyak */
+		BMETE11AX12, /* Bevezető fizika */
+		BMETE90AX40  /* Bevezető matematika */
+	},
+	TÚLCSORDULÓ CSOPORT KötelezőenVálasztható {
+		/* 2007/2008/1 félévig számító kötelezően választható tárgyak */
+		BMEGT20A002 (       - 2007/1), /* Marketing */
+		BMEGT20A017 (       - 2007/1), /* Innovációmenedzsment */
+		BMEGT33A002 (       - 2007/1), /* Marketing */
+		BMEGT35A001 (       - 2007/1), /* Pénzügyek */
+		BMEGT35A002 (       - 2007/1), /* Számvitel */
+		BMEGT35A003 (       - 2007/1), /* Gazdaságpolitika */
+		BMEGT41A001 (       - 2007/1), /* Filozófia */
+		BMEGT41A002 (       - 2007/1), /* Kutatásmódszertan */
+		BMEGT42A001 (       - 2007/1), /* Környezetgazdaságtan */
+		BMEGT42A002 (       - 2007/1), /* Regionális gazdaságtan */
+		BMEGT43A001 (       - 2007/1), /* Kommunikáció */
+		BMEGT43A002 (       - 2007/1), /* Szociológia */
+		BMEGT43A022 (       - 2007/1), /* Művészettörténet II. */
+		BMEGT44A001 (       - 2007/1), /* Innovációmenedzsment */
+		BMEGT46A001 (       - 2007/1), /* Információmenedzsment */
+		BMEGT47A001 (       - 2007/1), /* Látás, nyelv, emlékezet */
+		BMEGT51A001 (       - 2007/1), /* Digitális Pedagógia */
+		BMEGT52A001 (       - 2007/1), /* Ergonómia */
+		BMEGT52A002 (       - 2007/1), /* Pszichológia */
+		BMEVITMAK47 (       - 2007/1), /* Mérnöki menedzsment módszerek */
+		
+		/* 2007/2008/1 félévig számító kredites nyelvi tárgyak */
+		BMEGT61A051 (       - 2007/1), /* Műszaki nyelv - német */
+		BMEGT61A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - német */
+		BMEGT61A081 (       - 2007/1), /* Menedzserkommunikáció - német */
+		BMEGT61A091 (       - 2007/1), /* Kultúraközi kommunikáció - német */
+		BMEGT62AF51 (       - 2007/1), /* Műszaki nyelv - francia */
+		BMEGT62AF61 (       - 2007/1), /* Kommunikációs készségfejlesztés - francia */
+		BMEGT62AF81 (       - 2007/1), /* Menedzserkommunikáció – francia */
+		BMEGT62AF91 (       - 2007/1), /* Kultúraközi kommunikáció - francia */
+		BMEGT62AO51 (       - 2007/1), /* Műszaki nyelv - olasz */
+		BMEGT62AO61 (       - 2007/1), /* Kommunikációs készségfejlesztés - olasz */
+		BMEGT62AO81 (       - 2007/1), /* Menedzserkommunikáció - olasz */
+		BMEGT62AO91 (       - 2007/1), /* Kultúraközi kommunikáció - olasz */
+		BMEGT62AS61 (       - 2007/1), /* Kommunikációs készségfejlesztés - spanyol */
+		BMEGT62AS81 (       - 2007/1), /* Menedzserkommunikáció - spanyol */
+		BMEGT62AS91 (       - 2007/1), /* Kultúraközi kommunikáció - spanyol */
+		BMEGT63A051 (       - 2007/1), /* Műszaki nyelv - angol */
+		BMEGT63A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - angol */
+		BMEGT63A081 (       - 2007/1), /* Kultúraközi kommunikáció - angol */
+		BMEGT63A091 (       - 2007/1), /* Kultúraközi kommunikáció - angol */
+		BMEGT65A051 (       - 2007/1), /* Műszaki nyelv - magyar */
+		BMEGT65A061 (       - 2007/1), /* Kommunikációs készségfejlesztés - magyar */
+		BMEGT65A091 (       - 2007/1), /* Kultúraközi kommunikáció - magyar */
+		
+		/* 2007/2008/2 - 2010/2011/2 között érvényes tárgyak*/
+		BMEGT35A001 (2007/2 - 2010/2), /* Pénzügyek */
+		BMEGT47A001 (2007/2 - 2010/2), /* Látás, nyelv, emlékezet */
+		BMEGT35A003 (2007/2 - 2010/2), /* Gazdaságpolitika */
+		BMEGT42A001 (2007/2 - 2010/2), /* Környezetgazdaságtan */
+		BMEGT41A002 (2007/2 - 2010/2), /* Kutatásmódszertan */
+		BMEGT35A002 (2007/2 - 2010/2), /* Számvitel */
+		BMEVITMAK47 (2007/2 - 2010/2), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2007/2 - 2010/2), /* Marketing */
+		BMEGT52A002 (2007/2 - 2010/2), /* Pszichológia */
+		BMEGT52A001 (2007/2 - 2010/2), /* Ergonómia */
+		BMEGT43A002 (2007/2 - 2010/2), /* Szociológia */
+		BMEVIETAK49 (2007/2 - 2010/2), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2007/2 - 2010/2), /* Mérnöki problémamegoldás */
+		BMEGT51A001 (2007/2 - 2010/2), /* Digitális Pedagógia */
+		
+		/* 2011/2012/1 félévben érvényes tárgyak */
+		BMEGT35A001 (2011/1 - 2011/1), /* Pénzügyek */
+		BMEGT20V100 (2011/1 - 2011/1), /* Innovatív vállalkozások indítása és működtetése */
+		BMEGT35A003 (2011/1 - 2011/1), /* Gazdaságpolitika */
+		BMEGT42A001 (2011/1 - 2011/1), /* Környezetgazdaságtan */
+		BMEGT52A013	(2011/1 - 2011/1), /* Szociális készségfejlesztés */
+		BMEGT35A002 (2011/1 - 2011/1), /* Számvitel */
+		BMEVITMAK47 (2011/1 - 2011/1), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2011/1 - 2011/1), /* Marketing */
+		BMEGT52A002 (2011/1 - 2011/1), /* Pszichológia */
+		BMEGT52A001 (2011/1 - 2011/1), /* Ergonómia */
+		BMEGT43A002 (2011/1 - 2011/1), /* Szociológia */
+		BMEVIETAK49 (2011/1 - 2011/1), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2011/1 - 2011/1), /* Mérnöki problémamegoldás */
+		
+		/* 2011/2012/2 félévtől érvényes tárgyak */
+		BMEGT35A001 (2011/2 -       ), /* Pénzügyek */
+		BMEGT20V100 (2011/2 -       ), /* Innovatív vállalkozások indítása és működtetése */
+		BMEGT35A003 (2011/2 -       ), /* Gazdaságpolitika */
+		BMEGT42A001 (2011/2 -       ), /* Környezetgazdaságtan */
+		BMEGT52A013	(2011/2 -       ), /* Szociális készségfejlesztés */
+		BMEGT35A002 (2011/2 -       ), /* Számvitel */
+		BMEVITMAK47 (2011/2 -       ), /* Mérnöki menedzsment módszerek */
+		BMEGT20A002 (2011/2 -       ), /* Marketing */
+		BMEGT52A002 (2011/2 -       ), /* Pszichológia */
+		BMEGT52A001 (2011/2 -       ), /* Ergonómia */
+		BMEGT43A002 (2011/2 -       ), /* Szociológia */
+		BMEVIETAK49 (2011/2 -       ), /* Adatvédelem és információszabadság */
+		BMEVIVEAK48 (2011/2 -       ), /* Mérnöki problémamegoldás */
+		BMEVITMAK48 (2011/2 -       ), /* Érzelmek logikája */
+		BMEVITMAK49 (2011/2 -       ), /* Digitális életmód */
+		
+		/* Dénes féle kiegészítések, amik nincsenek fenn a kari honlapon */
+		BMEGT419021 (       - 2004/1), /* Érveléstechnika - logika */
+		BMEGT419733 (       - 2004/1), /* Filozófia */
+		BMEGT431427 (       - 2007/1), /* Művészettörténet II. (Impresszionizmustól a napjainkig) */
+		BMEGT628F91 (       - 2007/1), /* Francia nyelvi és civilizációs háttérismeretek - Filiere I. */
+		BMETE47A001 (       - 2010/2)  /* Látás, nyelv, emlékezet */
+	},
+	ALAPÉRTELMEZETT CSOPORT SzabadonVálasztható
+};
