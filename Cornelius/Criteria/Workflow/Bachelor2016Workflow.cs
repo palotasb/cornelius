ee4abc338ee84a7e456374a16ee0a57284a7055d
Palotás Boldizsár <boldizsar.palotas@gmail.com>
2016-05-21 11:46:00 +0200

    Create new Bachelor2016Workflow class and reference it in the workflow definition syntax and data file. The criteria is invalid as of yet.

---
+++ Cornelius/Criteria/Workflow/Bachelor2016Workflow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cornelius.Data;
+
+namespace Cornelius.Criteria.Workflow
+{
+    /// <summary>
+    /// A 2016 nyarától kezdődően érvényes feltételek szerinti besorolást elvégző workflow.
+    /// </summary>
+    class Bachelor2016Workflow : AbstractWorkflow
+    {
+        protected override void ProcessFinalResult(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
